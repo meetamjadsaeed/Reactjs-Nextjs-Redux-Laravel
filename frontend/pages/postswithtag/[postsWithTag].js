899eb11b1679a2bbb7e663b5e0ed90560b283dd5
Amjad Saeed <amjadsaeedmail@gmail.com>
2023-01-18 17:21:40 +0500

    optimize code

--- frontend/pages/postswithtag/[postsWithTag].js
+++ frontend/pages/postswithtag/[postsWithTag].js
@@ -1,145 +1,66 @@
-
-// import { useRouter } from 'next/router'
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router'
-import axios from "axios";
+import React, { useState } from 'react';
 import Head from 'next/head';
 import MainNav from '../../components/MainNav';
 import Footer from '../../components/Footer';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
-
 export default function postsWithTag(props) {
-  // console.log(props);
-  //   // const router = useRouter()
-  // // const id  = router.query.post;
-
   const [postsWithTag, setpostsWithTag] = useState(props.data.data);
-
-  // const router = useRouter();
-  
-  //       const slug  = router.query.post;
-
-  // const getData = async () => {
-  //       // Get Posts
-  //       const slug  = router.query.post;
-  //   await axios
-  //     .get(`http://127.0.0.1:8000/api/posts/${slug}`, {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization:
-  //           "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTBkNzRkYTQ3YjRkZWVlZmQzMTQwOGYxODQ1M2MzMmRlNmZmYTI1ZTQ4ZmMzN2MzMWMyOGEwMzljZWRlMDMwODllZTA1ZWM2ZGMzZWVjMGIiLCJpYXQiOjE2NjUzMzIxNDUuMzk0MTM2LCJuYmYiOjE2NjUzMzIxNDUuMzk0MTQzLCJleHAiOjE2OTY4NjgxNDUuMzg2NjUsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.soK3Hk6-oke4tpTujJAKyaRiG2jNx4bBwr05qQFNGYE4NawQON2QLTO6jbyzgVHDWu68QOsstQ_tbjyndzfvdyE7ePgyKgN6t4yuPLphMlE9PTXjAX4Rp1m-7W7gi4fkXs9AygfT8FH6RayZaoykDC_TaUM5DT5bJ1KsHOlEPzI2e4deBbualJfVRMPIL--ZDDAZEVjwCbDu2j9izX3kWTv3ZYseUKMWNcNh-F4xmjtZL0Cxo2TtehWUdS8b44zUAD3dsDIWBYP89ZgzsgITr2nuWVXEYulsFBiMQroliC-xr4g5AHzML6PxbK55jR5nTa7ow56UiDzA6QrFCWM58MNe6ZDKVClxk77QKCkOAhX5nH0cRTGf5ALCWnm10w0YmcH_ROh1EaEi0UQMK69S27KofMO4Giq1Qh3-j86mXnAANTD036B8GxBVWa_s86ALHLR6ZueGADyQOCStEOVgkaHShSxN2DFU0WN_RvudQmOxgGc0ulXem-qM0dikSm5AD2h-2QsCqzjcOU88hjk2KhDDsCgCbrD_bGNxhtFLJUWN9owvhRPxPeieq4hkfMbPKyITE6MEO7A9dQ4jvasnegpReR3kTmyAhCI2sjgQUfBwalYDQVVwfg88gHqzDPxdlxGOWfSpMF9rrRS2B8dNQnGcp7_dbOa9yCa-kMFALbg",
-  //       },
-  //     })
-  //     .then((result) => setpost(result.data.data));
-  //     // .then((result) => console.log(result));
-  // };
-
-  // //     fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a) => {
-  // //         return a.json();
-  // //     })
-  // //         .then((parsed) => {
-  // //             setBlog(parsed)
-  // //         })
-  // // }, [router.isReady])
-
-
-  // useEffect(() => {
-  //   if (!router.isReady) return;
-  //   getData();
-  // }, [router.isReady]);
- 
   return (
     <>
- <Head>
-        
-        </Head>
-  
-        {/* Header */}
-        <MainNav />
-
-      {/* <div>VeiwPost {id}</div> */}
-      {/* <p>{post.post_title}</p> */}
-
-<h1>Posts related to this category are</h1>
-
-<div className="container">
-   
-   <Row>
-  
- { postsWithTag ? 
-postsWithTag.map((item)=>{
- return (
-
-
-
-     <Col lg={4}>
-
-    <div class="card">
-   <ul>
-     <li>
-       <i class="fa-solid fa-mug-hot"></i>
-     </li>
-     <li>
-       <i class="fa-solid fa-film"></i>
-     </li>
-     <li>
-       <i class="fa-solid fa-store-alt"></i>
-     </li>
-     <li>
-       <i class="fa-solid fa-map"></i>
-     </li>
-   </ul>
-   <img
-     src="https://images.unsplash.com/photo-1518057111178-44a106bad636?ixlib=rb-1.2.1&w=1080&fit=max&q=80&fm=jpg&crop=entropy&cs=tinysrgb"
-     alt=""
-   />
-   <div class="content">
-     <h2>{item && item.title}</h2>
-     <p>
-       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus
-       labore laborum necessitatibus quis, earum, repellat facilis adipisci,
-       dolores corporis ipsam ratione eaque vitae quia sunt nemo et
-       repudiandae porro incidunt.
-       <button>See more...</button>
-     </p>
-   </div>
- </div>
-   
-     </Col>
-
-
-)
-}) : <p>loading...</p>
-}
-
-
-  
-</Row>
-   
-  
-   
- 
-   
-
-</div>
-
-      
-      {/* Footer */}
+      <Head>
+      </Head>
+      <MainNav />
+      <h1>Posts related to this category are</h1>
+      <div className="container">
+        <Row>
+          {postsWithTag ?
+            postsWithTag.map((item) => {
+              return (
+                <Col lg={4}>
+                  <div class="card">
+                    <ul>
+                      <li>
+                        <i class="fa-solid fa-mug-hot"></i>
+                      </li>
+                      <li>
+                        <i class="fa-solid fa-film"></i>
+                      </li>
+                      <li>
+                        <i class="fa-solid fa-store-alt"></i>
+                      </li>
+                      <li>
+                        <i class="fa-solid fa-map"></i>
+                      </li>
+                    </ul>
+                    <img
+                      src="https://images.unsplash.com/photo-1518057111178-44a106bad636?ixlib=rb-1.2.1&w=1080&fit=max&q=80&fm=jpg&crop=entropy&cs=tinysrgb"
+                      alt=""
+                    />
+                    <div class="content">
+                      <h2>{item && item.title}</h2>
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus
+                        labore laborum necessitatibus quis, earum, repellat facilis adipisci,
+                        dolores corporis ipsam ratione eaque vitae quia sunt nemo et
+                        repudiandae porro incidunt.
+                        <button>See more...</button>
+                      </p>
+                    </div>
+                  </div>
+                </Col>
+              )
+            }) :
+            <p>loading...</p>
+          }
+        </Row>
+      </div>
       <Footer />
-
-
     </>
   );
 }
 export async function getServerSideProps(context) {
-  // console.log(context);
-  // const router = useRouter();
-  
-  // const slug  = router.query.post;
-  const slug  = context.query.postsWithTag;
-
+  const slug = context.query.postsWithTag;
   const res = await fetch(` ${process.env.NEXT_PUBLIC_BACKEND_API}tags/${slug}/posts`, {
     headers: {
       "Content-Type": "application/json",
@@ -148,17 +69,7 @@ export async function getServerSideProps(context) {
     },
   })
   const data = await res.json()
-
-  // let data = await axios.get(`http://127.0.0.1:8000/api/posts/${slug}`, {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization:
-  //           "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTBkNzRkYTQ3YjRkZWVlZmQzMTQwOGYxODQ1M2MzMmRlNmZmYTI1ZTQ4ZmMzN2MzMWMyOGEwMzljZWRlMDMwODllZTA1ZWM2ZGMzZWVjMGIiLCJpYXQiOjE2NjUzMzIxNDUuMzk0MTM2LCJuYmYiOjE2NjUzMzIxNDUuMzk0MTQzLCJleHAiOjE2OTY4NjgxNDUuMzg2NjUsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.soK3Hk6-oke4tpTujJAKyaRiG2jNx4bBwr05qQFNGYE4NawQON2QLTO6jbyzgVHDWu68QOsstQ_tbjyndzfvdyE7ePgyKgN6t4yuPLphMlE9PTXjAX4Rp1m-7W7gi4fkXs9AygfT8FH6RayZaoykDC_TaUM5DT5bJ1KsHOlEPzI2e4deBbualJfVRMPIL--ZDDAZEVjwCbDu2j9izX3kWTv3ZYseUKMWNcNh-F4xmjtZL0Cxo2TtehWUdS8b44zUAD3dsDIWBYP89ZgzsgITr2nuWVXEYulsFBiMQroliC-xr4g5AHzML6PxbK55jR5nTa7ow56UiDzA6QrFCWM58MNe6ZDKVClxk77QKCkOAhX5nH0cRTGf5ALCWnm10w0YmcH_ROh1EaEi0UQMK69S27KofMO4Giq1Qh3-j86mXnAANTD036B8GxBVWa_s86ALHLR6ZueGADyQOCStEOVgkaHShSxN2DFU0WN_RvudQmOxgGc0ulXem-qM0dikSm5AD2h-2QsCqzjcOU88hjk2KhDDsCgCbrD_bGNxhtFLJUWN9owvhRPxPeieq4hkfMbPKyITE6MEO7A9dQ4jvasnegpReR3kTmyAhCI2sjgQUfBwalYDQVVwfg88gHqzDPxdlxGOWfSpMF9rrRS2B8dNQnGcp7_dbOa9yCa-kMFALbg",
-  //       },
-  //     })
-  //     .then((result));
-
   return {
-    props: { data }, // will be passed to the page component as props
-}
+    props: { data },
+  }
 }
