bdbd35cd4183a9002c48e30f789382bda7b78919
Amjad Saeed <amjadsaeedmail@gmail.com>
2023-03-04 22:23:34 +0500

    optimize

--- frontend/pages/posts/[post].js
+++ frontend/pages/posts/[post].js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router'
 import axios from "axios";
 import Head from 'next/head';
 import MainNav from '../../components/MainNav';
